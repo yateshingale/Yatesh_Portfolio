43fe29ee26c08d1400b778f5aba48e1ca6bb2f94
yateshingale <143589600+yateshingale@users.noreply.github.com>
2025-03-24 18:17:09 +0530

    adding projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import leaf from "../../Assets/Projects/Terraform.webp";
+import editor from "../../Assets/Projects/s3 python.webp";
+import chatify from "../../Assets/Projects/bitbucket cicd.webp";
+import bitsOfCode from "../../Assets/Projects/jenkins cicd.webp";
 
 function Projects() {
   return (
@@ -25,10 +23,13 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="BitBucket AutoDeploy 🤖
+
+"
+              description="This project automates the deployment of a Node.js application using Bitbucket Pipelines, 
+              ensuring seamless CI/CD integration on an AWS EC2 instance with PM2 for process management. 🚀"
+              ghLink="https://github.com/yateshingale/nodejs-app-deploy-bitbucket-project"
+              
             />
           </Col>
 
@@ -36,10 +37,11 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="🚀 Jenkins AutoDeploy"
+              description="This project showcases a Jenkins pipeline for automating build, test, and deployment workflows.
+               It integrates with Git, Docker, and cloud environments to enable seamless CI/CD automation. 🚀"
+              ghLink="https://github.com/yateshingale/jenkins-pipeline/tree/master"
+              
             />
           </Col>
 
@@ -47,10 +49,13 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="S3 Directory Scanner 📂
+
+"
+              description="This project features a **Python script** that retrieves and displays the directory structure of
+               an **AWS S3 bucket**. It helps in listing and organizing stored files efficiently. 📂🚀"
+              ghLink="https://github.com/yateshingale/s3_details"
+                           
             />
           </Col>
 
@@ -58,35 +63,16 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="S3 Bucket Analyzer With Terraform🛠️"
+              description="This project automates the retrieval and display of AWS S3 bucket details using Terraform, providing an
+               infrastructure-as-code (IaC) approach for managing and monitoring cloud storage configurations efficiently.
+                It simplifies cloud resource visibility and enhances automation in cloud operations."
+              ghLink="https://github.com/yateshingale/s3_detail_using_terraform"
+              
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
